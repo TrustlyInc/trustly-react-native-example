aec39cdbf145d41abca29204f0b629433c9289ca
Luke Vance <luke.vance@trustly.com>
2023-03-20 20:15:02 -0400

    webview is loading

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const handleLightboxWindowEvent = navState => {
+  console.log("event", navState);
+  if (navState.url) {
+    const baseUrls = ["paywithmybank.com", "trustly.one"];
+    const oauthLoginPath = "/oauth/login";
+    if (navState.url.includes(baseUrls[0]) || navState.url.includes(baseUrls[1])) {
+      console.log("lets open this thing!");
+    }
+  } else {
+    return null;
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Trustly!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <WebView 
+      source={{ uri: 'http://localhost:3000?integrationContext=InAppBrowser&urlScheme=trustly-react-native' }}
+      onNavigationStateChange={handleLightboxWindowEvent}
+      javaScriptEnabled={true}
+      startInLoadingState
+    />);
 }
 
 const styles = StyleSheet.create({
