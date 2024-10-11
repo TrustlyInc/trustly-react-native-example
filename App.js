07f6c22171871b415c0c6fe86ce6303aba04ab47
Renato DelPupo <16341269+renatodelpupo@users.noreply.github.com>
2024-10-10 21:48:12 -0300

    Add Trustly React Native SDK

--- App.js
+++ App.js
@@ -1,3 +1,27 @@
+/**
+ * @format
+ * @flow strict-local
+ */
+
+import { StyleSheet, View } from 'react-native';
+import TrustlyReactNativeSDK from 'trustly-react-native-sdk/js/TrustlyReactNativeSDKNativeComponent';
+
 export default function App() {
-  return;
+  return (
+    <View style={styles.container}>
+      <TrustlyReactNativeSDK
+        url='https://www.trustly.com'
+        style={{ height: '100%', width: '100%' }}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
