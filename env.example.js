dfaa2f1353107c58fec48e0848c685e8f918a2b4
Renato DelPupo <16341269+renatodelpupo@users.noreply.github.com>
2024-10-15 15:42:32 -0300

    Improve Establish Data setup

--- env.example.js
+++ env.example.js
@@ -4,5 +4,7 @@
  */
 
 export const ACCESS_ID = '<Provided by Trustly>'; // Required
+export const ENV = '';
+export const ENV_HOST = '';
 export const MERCHANT_ID = '<Provided by Trustly>'; // Required
 export const MERCHANT_REFERENCE = ''; // Required
--- establishData.js
+++ establishData.js
@@ -3,16 +3,44 @@
  * @flow strict
  */
 
-import { ACCESS_ID, MERCHANT_ID, MERCHANT_REFERENCE } from './env';
+import type { EstablishData } from './flow-typed/establish-data';
 
-export default {
+import {
+  ACCESS_ID,
+  ENV,
+  ENV_HOST,
+  MERCHANT_ID,
+  MERCHANT_REFERENCE,
+} from './env';
+
+const establishData: EstablishData = {
   accessId: ACCESS_ID,
-  'customer.address.country': 'us',
-  deviceType: 'desktop:macintel:web',
-  dynamicWidget: 'true',
-  grp: '34',
-  lang: 'en',
+  cancelUrl: '#',
+  currency: 'USD',
+  customer: {
+    address: {
+      address1: '2000 Broadway St',
+      city: 'Redwood City',
+      country: 'US',
+      state: 'CA',
+      zip: '94063',
+    },
+    enrollDate: 1704067200,
+    externalId: '123',
+    name: 'John',
+  },
+  description: 'React Native Example',
+  env: ENV,
+  envHost: ENV_HOST,
   merchantId: MERCHANT_ID,
   merchantReference: MERCHANT_REFERENCE,
+  metadata: {
+    lang: 'en',
+    urlScheme: '://',
+  },
   paymentType: 'Instant',
+  requestSignature: '',
+  returnUrl: '#',
 };
+
+export default establishData;
